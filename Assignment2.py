05c48af5aa7c1a5b44f109b64d37b4f7d0d8b4c4
Duc Minh Nguyen <2476537@edu.vaniercollege.qc.ca>
2025-11-13 20:55:45 -0500

    all questions finished

--- Assignment2.py
+++ Assignment2.py
@@ -71,10 +71,21 @@ def dot_product(list1, list2):
 # Function 7: Arithmetic Operations with Arrays - Matrix Multiplication
 # This function takes two 2D lists (matrices) and returns their matrix product.
 def matrix_multiplication(matrix1, matrix2):
-    return [[0, 0], [0, 0]]
+    rowsA = len(matrix1)
+    colsA = len(matrix1[0])
+    rowsB = len(matrix2)
+    colsB = len(matrix2[0])
+
+    if colsA != rowsB:
+        return("Numbers of rows in first matrix must equal number of columns of second matrix")
+
+    result = [[0] * colsB for i in range(rowsA)]
+
+    for i in range(rowsA):
+        for j in range(colsB):
+            for k in range(colsA):
+                result[i][j] += matrix1[i][k] * matrix2[k][j]
+
+    return result
 
 
-print(max_two_in_list([1, 3, 2, 5, 5]))
-
-
-5, 5, 3, 2, 1
